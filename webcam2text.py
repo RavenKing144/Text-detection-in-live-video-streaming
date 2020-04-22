41d97f598334ab110c90ef1e0d6f308ad41e7bba
Raven_king <32264219+RavenKing144@users.noreply.github.com>
2020-04-22 15:43:01 +0530

    Add files via upload

---
+++ webcam2text.py
@@ -0,0 +1,228 @@
+#It is capable of (1) running at near real-time at 13 FPS on 720p images and (2) obtains state-of-the-art text detection accuracy.
+#The EAST pipeline is capable of predicting words and lines of text at arbitrary orientations on 720p images, and furthermore, can run at 13 FPS, according to the authors.
+
+# import the necessary packages
+from imutils.video import VideoStream
+from imutils.video import FPS
+from imutils.object_detection import non_max_suppression
+import numpy as np
+import argparse
+import pytesseract
+import imutils
+import time
+import cv2
+
+def decode_predictions(scores, geometry):
+	# grab the number of rows and columns from the scores volume, then
+	# initialize our set of bounding box rectangles and corresponding
+	# confidence scores
+	(numRows, numCols) = scores.shape[2:4]
+	rects = []
+	confidences = []
+	# loop over the number of rows
+	for y in range(0, numRows):
+		# extract the scores (probabilities), followed by the
+		# geometrical data used to derive potential bounding box
+		# coordinates that surround text
+		scoresData = scores[0, 0, y]
+		xData0 = geometry[0, 0, y]
+		xData1 = geometry[0, 1, y]
+		xData2 = geometry[0, 2, y]
+		xData3 = geometry[0, 3, y]
+		anglesData = geometry[0, 4, y]
+		# loop over the number of columns
+		for x in range(0, numCols):
+			# if our score does not have sufficient probability,
+			# ignore it
+			if scoresData[x] < args["min_confidence"]:
+				continue
+			# compute the offset factor as our resulting feature
+			# maps will be 4x smaller than the input image
+			(offsetX, offsetY) = (x * 4.0, y * 4.0)
+			# extract the rotation angle for the prediction and
+			# then compute the sin and cosine
+			angle = anglesData[x]
+			cos = np.cos(angle)
+			sin = np.sin(angle)
+			# use the geometry volume to derive the width and height
+			# of the bounding box
+			h = xData0[x] + xData2[x]
+			w = xData1[x] + xData3[x]
+			# compute both the starting and ending (x, y)-coordinates
+			# for the text prediction bounding box
+			endX = int(offsetX + (cos * xData1[x]) + (sin * xData2[x]))
+			endY = int(offsetY - (sin * xData1[x]) + (cos * xData2[x]))
+			startX = int(endX - w)
+			startY = int(endY - h)
+			# add the bounding box coordinates and probability score
+			# to our respective lists
+			rects.append((startX, startY, endX, endY))
+			confidences.append(scoresData[x])
+	# return a tuple of the bounding boxes and associated confidences
+	return (rects, confidences)
+
+
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-east", "--east", type=str, required=True,
+	help="path to input EAST text detector")
+ap.add_argument("-v", "--video", type=str,
+	help="path to optinal input video file")
+ap.add_argument("-c", "--min-confidence", type=float, default=0.5,
+	help="minimum probability required to inspect a region")
+ap.add_argument("-w", "--width", type=int, default=320,
+	help="resized image width (should be multiple of 32)")
+ap.add_argument("-e", "--height", type=int, default=320,
+	help="resized image height (should be multiple of 32)")
+ap.add_argument("-p", "--padding", type=float, default=0.0,
+	help="amount of padding to add to each border of ROI")
+args = vars(ap.parse_args())
+
+
+# initialize the original frame dimensions, new frame dimensions,
+# and ratio between the dimensions
+(W, H) = (None, None)
+(newW, newH) = (args["width"], args["height"])
+(rW, rH) = (None, None)
+# define the two output layer names for the EAST detector model that
+# we are interested -- the first is the output probabilities and the
+# second can be used to derive the bounding box coordinates of text
+layerNames = [
+	"feature_fusion/Conv_7/Sigmoid",
+	"feature_fusion/concat_3"]
+# load the pre-trained EAST text detector
+print("[INFO] loading EAST text detector...")
+net = cv2.dnn.readNet(args["east"])
+
+
+
+# if a video path was not supplied, grab the reference to the web cam
+if not args.get("video", False):
+	print("[INFO] starting video stream...")
+	vs = VideoStream(src=0).start()
+	time.sleep(1.0)
+# otherwise, grab a reference to the video file
+else:
+	vs = cv2.VideoCapture(args["video"])
+# start the FPS throughput estimator
+fps = FPS().start()
+
+#OpenCV Text Detection (EAST text detector)
+# loop over frames from the video stream
+while True:
+	# grab the current frame, then handle if we are using a
+	# VideoStream or VideoCapture object
+	frame = vs.read()
+	frame = frame[1] if args.get("video", False) else frame
+	# check to see if we have reached the end of the stream
+	if frame is None:
+		break
+	# resize the frame, maintaining the aspect ratio
+	frame = imutils.resize(frame, width=1000)
+	orig = frame.copy()
+	# if our frame dimensions are None, we still need to compute the
+	# ratio of old frame dimensions to new frame dimensions
+	if W is None or H is None:
+		(H, W) = frame.shape[:2]
+		rW = W / float(newW)
+		rH = H / float(newH)
+	# resize the frame, this time ignoring aspect ratio
+	frame = cv2.resize(frame, (newW, newH))
+
+
+
+	# construct a blob from the frame and then perform a forward pass
+	# of the model to obtain the two output layer sets
+	blob = cv2.dnn.blobFromImage(frame, 1.0, (newW, newH),
+		(123.68, 116.78, 103.94), swapRB=True, crop=False)
+	net.setInput(blob)
+	(scores, geometry) = net.forward(layerNames)
+	# decode the predictions, then  apply non-maxima suppression to
+	# suppress weak, overlapping bounding boxes
+	(rects, confidences) = decode_predictions(scores, geometry)
+	boxes = non_max_suppression(np.array(rects), probs=confidences)
+	# loop over the bounding boxes
+	results = []
+	for (startX, startY, endX, endY) in boxes:
+		# scale the bounding box coordinates based on the respective
+		# ratios
+		startX = int(startX * rW)
+		startY = int(startY * rH)
+		endX = int(endX * rW)
+		endY = int(endY * rH)
+		# draw the bounding box on the frame
+		cv2.rectangle(orig, (startX, startY), (endX, endY), (0, 255, 0), 2)
+		# in order to obtain a better OCR of the text we can potentially
+		# apply a bit of padding surrounding the bounding box -- here we
+		# are computing the deltas in both the x and y 
+
+		dX = int((endX - startX) * args["padding"])
+		dY = int((endY - startY) * args["padding"])
+		# apply padding to each side of the bounding box, respectively
+		startX = max(0, startX - dX)
+		startY = max(0, startY - dY)
+		endX = min(W, endX + (dX * 2))
+		endY = min(H, endY + (dY * 2))
+
+		# extract the actual padded ROI
+		roi = orig[startY:endY, startX:endX]
+		# in order to apply Tesseract v4 to OCR text we must supply
+		# (1) a language, (2) an OEM flag of 4, indicating that the we
+		# wish to use the LSTM neural net model for OCR, and finally
+		# (3) an OEM value, in this case, 7 which implies that we are
+		# treating the ROI as a single line of text
+		config = ("-l eng --oem 1 --psm 7")
+		text = pytesseract.image_to_string(roi, config=config)
+
+	# add the bounding box coordinates and OCR'd text to the list
+	# of results
+		results.append(((startX, startY, endX, endY), text))
+		# sort the results bounding box coordinates from top to bottom
+		results = sorted(results, key=lambda r:r[0][1])
+		for ((startX, startY, endX, endY), text) in results:
+		# display the text OCR'd by Tesseract
+			print("OCR TEXT")
+			print("========")
+			print("{}\n".format(text))
+
+		# strip out non-ASCII text so we can draw the text on the image
+		# using OpenCV, then draw the text and a bounding box surrounding
+		# the text region of the input image
+			text = "".join([c if ord(c) < 128 else "" for c in text]).strip()
+			output = orig.copy()
+			cv2.rectangle(output, (startX, startY), (endX, endY),
+				(0, 0, 255), 2)
+			cv2.putText(output, text, (startX, startY - 20),
+				cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 255), 3)
+
+			# show the output image
+			cv2.imshow("Text Detection", output)
+			cv2.waitKey(0)
+
+
+		
+		#cv2.imshow("",orig[startX:endX+1,startY:endY+1])z
+
+		# update the FPS counter
+	fps.update()
+	# show the output frame
+	cv2.imshow("Text Detection", orig)
+	key = cv2.waitKey(1) & 0xFF
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
+	if key == ord("z"):
+		exit()
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+# if we are using a webcam, release the pointer
+if not args.get("video", False):
+	vs.stop()
+# otherwise, release the file pointer
+else:
+	vs.release()
+# close all windows
+cv2.destroyAllWindows()
